4c6a4d8127d280dee61016cfcee76102c3b2f641
risetechapps <apps@risetech.com.br>
2025-04-19 00:20:13 -0300

    Registrado macro

--- src/OrchestratorLinkServiceProvider.php
+++ src/OrchestratorLinkServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace RiseTechApps\OrchestratorLink;
 
+use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\ServiceProvider;
 
 class OrchestratorLinkServiceProvider extends ServiceProvider
@@ -16,6 +17,8 @@ class OrchestratorLinkServiceProvider extends ServiceProvider
                 __DIR__ . '/../config/config.php' => config_path('orchestrator-link.php'),
             ], 'config');
         }
+
+        $this->registerMacros();
     }
 
     /**
@@ -28,5 +31,42 @@ class OrchestratorLinkServiceProvider extends ServiceProvider
         $this->app->singleton(OrchestratorLink::class, function () {
             return new OrchestratorLink;
         });
+    }
+
+    protected function registerMacros(): void
+    {
+
+        if(!ResponseFactory::hasMacro('jsonSuccess')){
+            ResponseFactory::macro('jsonSuccess', function ($data = []) {
+                $response = ['success' => true];
+                if (!empty($data)) $response['data'] = $data;
+                return response()->json($response);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonError')){
+            ResponseFactory::macro('jsonError', function ($data = null) {
+                $response = ['success' => false];
+                if (!is_null($data)) $response['message'] = $data;
+                return response()->json($response, 412);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonGone')) {
+            ResponseFactory::macro('jsonGone', function ($data = null) {
+                $response = ['success' => false];
+                if (!is_null($data)) $response['message'] = $data;
+                return response()->json($response, 410);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonNotValidated')) {
+            ResponseFactory::macro('jsonNotValidated', function ($message = null, $error = null) {
+                $response = ['success' => false];
+                if (!is_null($message)) $response['message'] = $message;
+
+                return response()->json($response, 422);
+            });
+        }
     }
 }
